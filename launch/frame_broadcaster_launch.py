afe476c420346034f914553f820777b27c6e9ecf
Abhishek-EE <abhishekranjansingh014@gmail.com>
2024-01-27 12:11:06 -0600

    frame_broadcaster_updated

--- launch/frame_broadcaster_launch.py
+++ launch/frame_broadcaster_launch.py
@@ -5,18 +5,33 @@ import yaml
 import os
 
 def generate_launch_description():
-    # Load the frames.yaml file
-    # config_directory = os.path.join(get_package_share_directory('sensor_integration_suite'), 'config')
-    config_directory = os.path.join(get_package_share_directory('sensor_integration_suite'))
-    frames_yaml = os.path.join(config_directory, 'frames.yaml')
-    # with open(frames_yaml, 'r') as file:
-    #     frames_config = yaml.safe_load(file)
+    # Load the YAML file with transform configurations
+    config_directory = os.path.join(get_package_share_directory('your_package_name'), 'config')
+    yaml_file = os.path.join(config_directory, 'transforms.yaml')
+    
+    with open(yaml_file, 'r') as file:
+        transforms_config = yaml.safe_load(file)['transforms']
 
-    return LaunchDescription([
-        Node(
-            package='sensor_integration_suite',
-            executable='frame_broadcaster',
-            name='frame_broadcaster_node',
-            parameters=[{'frames_yaml':frames_yaml}],
+    # Create a list of Node actions from the loaded configurations
+    transform_publishers = []
+    for transform in transforms_config:
+        translation = transform['translation']
+        rotation = transform['rotation']
+        parent_frame = transform['parent_frame']
+        child_frame = transform['child_frame']
+
+        arguments = [
+            str(translation[0]), str(translation[1]), str(translation[2]),
+            str(rotation[0]), str(rotation[1]), str(rotation[2]),
+            parent_frame, child_frame
+        ]
+
+        transform_publishers.append(
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                arguments=arguments
+            )
         )
-    ])
+
+    return LaunchDescription(transform_publishers)
