04e06c42ef42db306622a97c56191afb932678c7
Abhishek-EE <abhishekranjansingh014@gmail.com>
2024-02-01 12:38:44 -0600

    frame_info_change

--- launch/dual_lidar.launch.py
+++ launch/dual_lidar.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             name='lidar_publisher_node_hz',
             parameters=[{'lidar_uri': '/dev/ttyUSB0', 
                          'topic_name': '/lidar/points_hz',
-                         'frame_id':'horizontal_lidar_link'}],
+                         'frame_id':'horizontal_laser_link'}],
             output='screen'
         ),
         # Launch the second LidarPublisherNode
@@ -20,7 +20,7 @@ def generate_launch_description():
             name='lidar_publisher_node_vt',
             parameters=[{'lidar_uri': '/dev/ttyUSB1', 
                          'topic_name': '/lidar/points_vt',
-                         'frame_id':'vertical_lidar_link'}],
+                         'frame_id':'vertical_laser_link'}],
             output='screen'
         )
     ])
